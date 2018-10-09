37464ae74ba4632fee0a74b8ca090a8f6fc586c9
Rishav Krishna <rishavkrishna22@gmail.com>
2018-10-09 21:50:52 +0530

    Update collapse_using_php.php

--- collapse_using_php.php
+++ collapse_using_php.php
@@ -11,6 +11,7 @@
 
 </style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	</head>
 <script>
     $(document).ready(function(){
         $('.slide').hide();
